d83fcbdd5e76c72eecb4a5342c7234ac249ea309
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2017-10-09 22:37:34 -0400

    Added input to enter connection data

--- react_components/sshconnect.js
+++ react_components/sshconnect.js
@@ -6,27 +6,104 @@ var React = require("react"),
     request = require("superagent");
 
 module.exports = React.createClass({
-    componentWillMount: function() {
-    request
-        .post("/api/cmd")
-        .send({
-            ip: "192.168.1.188",
-            port: "22",
-            username: "pi",
-            password: "raspberry"
-        })
-        .end(function(err, res) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(res.body)
-            }
-        }.bind(this));
-},
+    getInitialState: function() {
+        return {
+            sshresult: "test"
+        };
+    },
+    handleSubmit: function(e) {
+        e.preventDefault();
+        request
+            .post("/api/cmd")
+            .send({
+                ip: this.refs.ip.value.trim(),
+                port: this.refs.port.value.trim(),
+                username: this.refs.username.value.trim(),
+                password: this.refs.password.value.trim(),
+                cmd: this.refs.cmd.value.trim()
+            })
+            .end(function(err, res) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    this.setState({
+						sshresult: res.body
+					});
+                }
+            }.bind(this));
+
+        return false;
+    },
     render: function() {
         return React.DOM.div(
             null,
-            "test"
+            React.DOM.form({
+                    name: "form",
+                    noValidate: "",
+                    onSubmit: this.handleSubmit
+                },
+                React.DOM.div({
+                        className: "form-group"
+                    },
+                    React.DOM.label(null, "Username"),
+                    React.DOM.input({
+                        className: "form-control",
+                        type: "text",
+                        ref: "username",
+                        name: "username",
+                        defaultValue: "root",
+                        required: true
+                    })
+                ),
+                React.DOM.div({
+                        className: "form-group"
+                    },
+                    React.DOM.label(null, "Password"),
+                    React.DOM.input({
+                        className: "form-control",
+                        type: "password",
+                        ref: "password",
+                        name: "password"
+                    })
+                ),
+                React.DOM.div({
+                        className: "form-group"
+                    },
+                    React.DOM.label(null, "IP"),
+                    React.DOM.input({
+                        className: "form-control",
+                        type: "text",
+                        ref: "ip",
+                        name: "ip",
+                        defaultValue: "192.168.1.1",
+                        required: true
+                    })
+                ),
+                React.DOM.div({
+                        className: "form-group"
+                    },
+                    React.DOM.label(null, "Command"),
+                    React.DOM.input({
+                        className: "form-control",
+                        type: "text",
+                        ref: "cmd",
+                        name: "cmd",
+                        defaultValue: "ls",
+                        required: true
+                    })
+                ),
+                React.DOM.div({
+                    className: "ssh-response"
+                }, ),
+                React.DOM.button({
+                    className: "btn btn-lg btn-primary btn-block",
+                    type: "submit"
+                }, "Submit")
+            ),
+            React.DOM.div(
+                null,
+                this.state.sshresult
+            )
         );
     }
 });
