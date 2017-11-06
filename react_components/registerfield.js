52b502dd7caf5bee3ad24f1465d605d3b4dfc4f0
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2017-11-05 23:09:15 -0500

    Fisrt version to display correctly field name verticaly.

--- react_components/registerfield.js
+++ react_components/registerfield.js
@@ -7,50 +7,77 @@ var React = require("react"),
 
 module.exports = React.createClass({
     componentWillReceiveProps: function(nextProps) {
-        var arrayIndex = 0;
-        _.forEach(nextProps.parent, function(data) {
-            for (var i = 0; i < data.size; i += 1) {
-                this.state.value[arrayIndex + i] = data.name;
-            }
-            arrayIndex += data.size;
-        }.bind(this));
+        var tmp = _.map(nextProps.parent, function(field, index) {
+            return React.DOM.div(
+                {
+                    className: "field-detail-bit-detail1 table-bordered",
+                    key: index,
+                    style: {
+                        width: (35 * field.size) + "px"
+                    }
+                },
+                React.DOM.div({
+                    className: "field-detail-bit-detail"
+                }, field.name)
+            );
+        });
 
-        this.forceUpdate();
+        this.setState({
+            value: tmp
+        });
     },
     getInitialState: function() {
+        var value = [];
+
+        for (let i = 0; i < 7; i += 1) {
+            var tmp = React.DOM.div({
+                    className: "field-detail-bit-detail table-bordered",
+                    key: i,
+                    style: {
+                        width: "200px",
+                        marginTop: "-35px"
+                    }
+                }, "Description field");
+
+            value.push(tmp);
+        }
+
         return {
-            value: _.fill(Array(8), "Description field")
+            value: value
         };
     },
     renderColumn: function(index) {
-        return React.DOM.div(
-            {
-                className: "field-detail-column",
+        return React.DOM.div({
+                className: "field-detail-bit-index table-bordered text-center text-bold",
                 key: index
-            }, React.DOM.div({
-                className: "table-bordered text-center text-bold"
-            }, index),
-            React.DOM.div({
-                className: "field-detail-bit-detail table-bordered",
-                onClick: this.handleClick.bind(this, index)
-            }, this.state.value[index])
-        );
+            }, index);
     },
     handleClick: function(index) {
         this.props.callBack(index);
 	},
     render: function() {
-        var data = [];
+        var indexDiv = [];
 
-        for (var i = 7; i >= 0; i -= 1) {
-            data.push(this.renderColumn(i));
+        for (let i = 7; i >= 0; i -= 1) {
+            indexDiv.push(this.renderColumn(i));
         }
 
         return React.DOM.div(
             {
                 className: "text-center"
             },
-            data
+            React.DOM.div(
+                {
+                    className: "register-field-container"
+                },
+                indexDiv
+            ),
+            React.DOM.div(
+                {
+                    className: "field-detail-bit-detail-container text-center"
+                },
+                this.state.value
+            )
         );
     }
 });
