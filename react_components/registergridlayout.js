809a28fa97d573b4817006b60402fd8335b3a2a6
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2017-11-17 17:19:50 -0500

    Improve register grid layout.

--- react_components/registergridlayout.js
+++ react_components/registergridlayout.js
@@ -4,39 +4,69 @@ var React = require("react"),
     _ = require("lodash");
 
 var toHex = function(data) {
-    if (Number.isInteger(data)) {
-        var hexValue = data.toString(16).toUpperCase();
-        if (hexValue.length < 2) {
-            hexValue = "0" + hexValue;
-        }
-        return hexValue;
+    if (data === "-") {
+        return data;
     }
-    return "00";
+
+    if (Number.isInteger(data.substring(2))) {
+        return data.substring(2);
+    }
+
+    return "-";
 };
 
 module.exports = React.createClass({
+    handleClick: function(offset) {
+        this.props.callBack(offset);
+    },
     render: function() {
+        var max = _.maxBy(this.props.data, function(data) {
+            return data.offset;
+        });
+
+        var gridData = _.fill(Array(max.offset + 1), "-");
+
+        _.forEach(this.props.data, function(data, index) {
+            gridData[data.offset] = this.props.readData[index];
+        }.bind(this));
+
+        var header = [];
+
+        for (var i = 0; i < 16; i += 1) {
+            header.push(React.DOM.div(
+                {
+                    className: "registergrid-item text-bold",
+                    key: i
+                },
+                "0" + i.toString(16).toUpperCase()
+            ));
+        }
         return React.DOM.div(
             {
                 className: "registergridlayout"
             },
-            _.map(_.chunk(this.props.readData, 10), function(line, key) {
+            React.DOM.div({
+                    className: "text-center",
+                    key: 0
+                }, header),
+            _.map(_.chunk(gridData, 16), function(line, lineIdx) {
                 return React.DOM.div(
                     {
                         className: "text-center",
-                        key: key
+                        key: lineIdx
                     },
-                    _.map(line, function(data, index) {
+                    _.map(line, function(data, columnIdx) {
                         return React.DOM.div(
                             {
                                 className: "registergrid-item",
-                                key: index
+                                key: columnIdx,
+                                onClick: this.handleClick.bind(this, columnIdx + lineIdx)
                             },
                             toHex(data)
                         );
-                    })
+                    }.bind(this))
                 );
-            })
+            }.bind(this))
         );
     }
 });
