f29f4e5cacc1be967811127c9c45dad1f5735c55
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2018-07-19 22:52:44 -0400

    Apply AirBnb #2

--- database.js
+++ database.js
@@ -3,8 +3,8 @@ var mongoClient = require('mongodb').MongoClient;
 module.exports.connect = function () {
     var promise = mongoClient.connect('mongodb://localhost:27017/sshcmd');
 
-	return promise.then(function (database) {
-		module.exports.instance = database;
-		module.exports.users = database.collection('users');
-	});
+    return promise.then((database) => {
+        module.exports.instance = database;
+        module.exports.users = database.collection('users');
+    });
 };
