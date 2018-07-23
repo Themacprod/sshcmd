7e6dc8bdd1360be91d3f6745a7af88e370bbac33
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2018-07-22 21:34:14 -0400

    Removed unecessary state for dropdown

--- react_components/dropdown.js
+++ react_components/dropdown.js
@@ -2,29 +2,15 @@ var React = require('react'),
     _ = require('lodash');
 
 module.exports = React.createClass({
-    handleButtonClick: function () {
-        this.setState({
-            showDropdown: !this.state.showDropdown
-        });
-    },
     handleItemClick: function (index, value) {
         this.props.callBack(index, value);
-        this.setState({
-            showDropdown: false
-        });
-    },
-    getInitialState: function () {
-        return {
-            showDropdown: false
-        };
     },
     render: function () {
-        const showDropdown = this.state.showDropdown ? ' show' : '';
         const that = this;
 
         return React.DOM.div(
             {
-                className: `dropdown${showDropdown}`
+                className: 'dropdown'
             },
             React.DOM.button(
                 {
@@ -33,14 +19,13 @@ module.exports = React.createClass({
                     id: this.props.id,
                     'data-toggle': 'dropdown',
                     'aria-haspopup': 'true',
-                    'aria-expanded': this.state.showDropdown,
                     onClick: this.handleButtonClick
                 },
                 this.props.current
             ),
             React.DOM.div(
                 {
-                    className: `dropdown-menu${showDropdown}`,
+                    className: 'dropdown-menu',
                     'aria-labelledby': this.props.id
                 },
                 _.map(this.props.values, (value, index) => {
